cd8ef6f4c61881cdc1d5b80e578e34e2c0a2802b
Magomed Alimkhanov <scrythepvp@gmail.com>
2023-12-19 22:06:09 +0100

    fix: change back to old and make lysten sync

--- src/index.js
+++ src/index.js
@@ -1,50 +1,15 @@
-"use strict";
-var __create = Object.create;
-var __defProp = Object.defineProperty;
-var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-var __getOwnPropNames = Object.getOwnPropertyNames;
-var __getProtoOf = Object.getPrototypeOf;
-var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __copyProps = (to, from, except, desc) => {
-  if (from && typeof from === "object" || typeof from === "function") {
-    for (let key of __getOwnPropNames(from))
-      if (!__hasOwnProp.call(to, key) && key !== except)
-        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-  }
-  return to;
-};
-var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
-  // If the importer is in node compatibility mode or this is not an ESM
-  // file that has been converted to a CommonJS file using a Babel-
-  // compatible transform (i.e. "__esModule" has not been set), then set
-  // "default" to the CommonJS "module.exports" for node compatibility.
-  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
-  mod
-));
+const Fastify = require("fastify");
+const port = process.env.PORT | 3000;
 
-// src/index.ts
-var import_fastify = __toESM(require("fastify"));
-var port = process.env.PORT || 3e3;
-var enviroment = process.env.enviroment || "development";
-var envToLogger = {
-  production: true,
-  development: {
-    transport: {
-      target: "pino-pretty",
-      options: {
-        translateTime: "HH:MM:ss Z",
-        ignore: "pid,hostname"
-      }
-    }
-  }
-};
-var fastify = (0, import_fastify.default)({
-  logger: envToLogger[enviroment] || true
+const fastify = Fastify({
+  logger: true,
 });
-fastify.get("/", () => {
+
+fastify.get("/", function handler() {
   return { message: "Hello World!" };
 });
-var start = async () => {
+
+const listen = async () => {
   try {
     fastify.listen({ port, host: "0.0.0.0" });
   } catch (err) {
@@ -52,4 +17,5 @@ var start = async () => {
     process.exit(1);
   }
 };
-start();
+
+listen();
