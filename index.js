2dec1f8fb9fbf49d0c5e09f99656aa6e0b6af691
BlackDragDroit <gabrielgeyrhofer@gmail.com>
2023-12-19 20:31:20 +0100

    Refactor variable names and update comments

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Fastify = require("fastify");
+const port = process.env.PORT | 3000;
+
 const fastify = Fastify({
   logger: true,
 });
@@ -9,7 +11,7 @@ fastify.get("/", async function handler(request, reply) {
 
 async function listen() {
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ port, host: "0.0.0.0" });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
